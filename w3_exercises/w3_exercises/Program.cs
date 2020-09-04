879d05cc50712996939f137644449473c82e78cd
Veltosian <wizardnep@gmail.com>
2020-09-04 15:28:13 -0700

    Initial commit of w3_exercises project for practice with w3resource.com c# exercises

---
+++ w3_exercises/w3_exercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace w3_exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
