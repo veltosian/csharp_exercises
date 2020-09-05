1a2671946b4322c975094e73e6b94ec15712fd15
veltosian <wizardnep@gmail.com>
2020-09-05 07:41:17 -0700

    Added Tests class

--- w3_exercises/w3_exercises/Program.cs
+++ w3_exercises/w3_exercises/Program.cs
@@ -7,6 +7,7 @@ namespace w3_exercises
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello emacs!");
         }
     }
 }
---
+++ w3_exercises/w3_exercises/Tests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace w3_exercises
+{
+    class Tests
+    {
+    }
+}
